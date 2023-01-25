a722643018f77d1d424ccf994fa0522f7a3d3817
anupkumar999 <anupk3518@gmail.com>
2023-01-25 10:33:30 +0530

    this is my react app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>HELLO SIR</h1>
         </a>
       </header>
     </div>
